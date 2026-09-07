c1904ed80edd8e46e1fbf3fa879fbccbb543237c
Quique <quiqueb2006@gmail.com>
2026-09-06 20:06:08 -0600

    Crear un único StoreViewModel con StateFlow

---
+++ app/src/main/java/com/example/laboratorio_09_tienda_temtica/ui/StoreViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.laboratorio_09_tienda_temtica.ui
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class StoreViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(StoreUiState())
+    val uiState: StateFlow<StoreUiState> = _uiState.asStateFlow()
+}
